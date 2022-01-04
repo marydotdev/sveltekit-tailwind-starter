6cee2a686a45ba0528b4816f744b5a9a9ac4e314
Mary Haedrich <mary@Marys-Mac-mini.local>
2022-01-04 12:21:24 -0500

    add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,19 @@
 import adapter from '@sveltejs/adapter-auto';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	kit: {
 		adapter: adapter(),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},
+
+	preprocess: [mdsvex(mdsvexConfig)]
 };
 
 export default config;
